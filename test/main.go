478cb7e807b56b9023d5080504ad16b913c294d0
Karan <pal.karan2002@gmail.com>
2023-12-18 23:38:05 +0530

    Update main_test.go
    added endpoints for testing using Postman

--- test/main.go
+++ test/main.go
@@ -1,160 +1,101 @@
-// main.go
-
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	_ "github.com/mattn/go-sqlite3"
-	"gofr.dev/pkg/gofr"
-	"log"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
 )
 
-// Tile represents a tile in the inventory.
-type Tile struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Stock int    `json:"stock"`
-}
-
-// InventoryService represents the microservice for inventory management.
-type InventoryService struct {
-	Tiles map[string]Tile
-	DB    *sql.DB
-}
-
-func (s *InventoryService) Initialize() {
-	s.Tiles = make(map[string]Tile)
-
-	// Initialize SQLite3 database
-	db, err := sql.Open("sqlite3", "./tiles.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	s.DB = db
-
-	// Create the 'tiles' table if not exists
-	createTable := `
-CREATE TABLE IF NOT EXISTS tiles (
-    id TEXT PRIMARY KEY,
-    name TEXT,
-    stock INTEGER
-);
-`
-	_, err = db.Exec(createTable)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-// CreateTileHandler handles the creation of a new tile.
-func (s *InventoryService) CreateTileHandler(ctx *gofr.Context) (interface{}, error) {
-	var tile Tile
-	if err := ctx.BodyParser(&tile); err != nil {
-		return nil, err
-	}
-
-	// Generate a unique ID
-	tile.ID = fmt.Sprintf("%d", len(s.Tiles)+1)
-
-	// Insert into SQLite3 database
-	_, err := s.DB.Exec("INSERT INTO tiles (id, name, stock) VALUES (?, ?, ?)", tile.ID, tile.Name, tile.Stock)
-	if err != nil {
-		return nil, err
-	}
-
-	s.Tiles[tile.ID] = tile
-	return tile, nil
-}
-
-// UpdateStockHandler handles the update of tile stock.
-func (s *InventoryService) UpdateStockHandler(ctx *gofr.Context) (interface{}, error) {
-	tileID := ctx.PathParam("id")
-
-	// Assuming a positive or negative integer is sent in the request body for stock update
-	var stockUpdate int
-	if err := ctx.BodyParser(&stockUpdate); err != nil {
-		return nil, err
-	}
-
-	tile, found := s.Tiles[tileID]
-	if !found {
-		return nil, fmt.Errorf("tile %v not found", tileID)
-	}
-
-	// Update stock
-	tile.Stock += stockUpdate
-	s.Tiles[tileID] = tile
-
-	// Update SQLite3 database
-	_, err := s.DB.Exec("UPDATE tiles SET stock=? WHERE id=?", tile.Stock, tileID)
-	if err != nil {
-		return nil, err
-	}
-
-	return tile, nil
-}
-
-// ReadTilesHandler handles reading all available tiles.
-func (s *InventoryService) ReadTilesHandler(ctx *gofr.Context) (interface{}, error) {
-	tiles := make([]Tile, 0, len(s.Tiles))
-	rows, err := s.DB.Query("SELECT * FROM tiles")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var tile Tile
-		err := rows.Scan(&tile.ID, &tile.Name, &tile.Stock)
-		if err != nil {
-			return nil, err
+func TestCRUDOperations(t *testing.T) {
+	app := fiber.New()
+	// Adding a cors for put and delete request	
+	app.Use(func(c *fiber.Ctx) error {
+		c.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		return c.Next()
+	})
+	app.Get("/tiles", getTiles)
+	app.Get("/tiles/:id", getTile)
+	app.Post("/tiles", createTile)
+	app.Put("/tiles/:id", updateTile)
+	app.Delete("/tiles/:id", deleteTile)
+	
+	t.Run("CreateTile", func(t *testing.T) {
+		// Define a sample tile for testing
+		tile := Tile{
+			Name:         "Kajaria",
+			ID:			  "1",
+			Stock:		  "10",
 		}
-		tiles = append(tiles, tile)
-	}
 
-	return tiles, nil
+		// Convert tile to JSON
+		tileJSON, err := json.Marshal(tile)
+		assert.NoError(t, err)
+
+		// Create a POST request with the tile JSON
+		req := httptest.NewRequest(http.MethodPost, "/tiless", bytes.NewReader(tileJSON))
+		req.Header.Set("Content-Type", "application/json")
+
+		// Create a response recorder to record the response
+		res, err := app.Test(req)
+		assert.NoError(t, err)
+
+		// Assert the status code is 200 OK
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+	})
+
+	t.Run("GetTiles", func(t *testing.T) {
+		// Create a GET request to retrieve all tiles
+		req := httptest.NewRequest(http.MethodGet, "/tiles", nil)
+
+		// Create a response recorder to record the response
+		res, err := app.Test(req)
+		assert.NoError(t, err)
+
+		// Assert the status code is 200 OK
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+
+	})
+
+	
+	t.Run("UpdateTile", func(t *testing.T) {
+		// Define a sample tile for testing
+		updatedTile := Tile{
+			Name:         "Somany",
+			ID:			  "2",
+			Stock:		  "100",
+			
+		}
+
+		// Convert updated tile to JSON
+		updatedTileJSON, err := json.Marshal(updatedTile)
+		assert.NoError(t, err)
+
+		// Create a PUT request with the updated tile JSON
+		req := httptest.NewRequest(http.MethodPut, "/tiles/657ad262660b9c2342474c33", bytes.NewReader(updatedTileJSON))
+		req.Header.Set("Content-Type", "application/json")
+
+		// Create a response recorder to record the response
+		res, err := app.Test(req)
+		assert.NoError(t, err)
+
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+	})
+
+	t.Run("DeleteTile", func(t *testing.T) {
+		// Create a DELETE request to delete a tile
+		req := httptest.NewRequest(http.MethodDelete, "/tiles/657be72ea13c844c358f83b2", nil)
+
+		// Create a response recorder to record the response
+		res, err := app.Test(req)
+		assert.NoError(t, err)
+
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+	})
+
 }
 
-// DeleteTileHandler handles deleting finished stock from the database.
-func (s *InventoryService) DeleteTileHandler(ctx *gofr.Context) (interface{}, error) {
-	tileID := ctx.PathParam("id")
 
-	// Check if the tile exists
-	_, found := s.Tiles[tileID]
-	if !found {
-		return nil, fmt.Errorf("tile %v not found", tileID)
-	}
-
-	// Delete from SQLite3 database
-	_, err := s.DB.Exec("DELETE FROM tiles WHERE id=?", tileID)
-	if err != nil {
-		return nil, err
-	}
-
-	// Delete the tile
-	delete(s.Tiles, tileID)
-
-	return map[string]string{"message": "Tile deleted successfully"}, nil
-}
-
-func main() {
-	app := gofr.New()
-
-	// Initialize the InventoryService
-	inventoryService := &InventoryService{}
-	inventoryService.Initialize()
-
-	// Register handlers
-	app.POST("/tiles", inventoryService.CreateTileHandler)
-	app.PUT("/tiles/:id/stock", inventoryService.UpdateStockHandler)
-	app.GET("/tiles", inventoryService.ReadTilesHandler)
-	app.DELETE("/tiles/:id", inventoryService.DeleteTileHandler)
-
-	// Specify the port for the server to listen on
-	port := 8080
-	app.Start()
-
-	// Start the server
-	fmt.Printf("Server is running on port %d...\n", port)
-}
