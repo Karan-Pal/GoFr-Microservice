e679247a4e276060f3177ef0e25bb3b873cb37b4
Karan <pal.karan2002@gmail.com>
2023-12-18 23:39:59 +0530

    Update main.go
    changed the database to mongoDB due to some connectivity issues with sqlite.

--- main.go
+++ main.go
@@ -3,10 +3,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"github.com/gocql/gocql"
-	"gofr.dev/pkg/gofr"
 	"log"
+	"time"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Tile represents a tile in the inventory.
@@ -21,99 +26,149 @@ type InventoryService struct {
 	Tiles map[string]Tile
 }
 
-// Initialize initializes the inventory service.
-func (s *InventoryService) Initialize() {
-	s.Tiles = make(map[string]Tile)
-}
+var client *mongo.Client
 
-// CreateTileHandler handles the creation of a new tile.
-func (s *InventoryService) CreateTileHandler(ctx *gofr.Context) (interface{}, error) {
-	var tile Tile
-	if err := ctx.WebSocketConnection.ReadJSON(&tile); err != nil {
-		return nil, err
-	}
+// Connect to MongoDB
+func init() {
+	// Set up client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
-	// Generate a unique ID (you might want to use a more robust method)
-	tile.ID = fmt.Sprintf("%d", len(s.Tiles)+1)
-
-	s.Tiles[tile.ID] = tile
-	return tile, nil
-}
-
-// UpdateStockHandler handles the update of tile stock.
-func (s *InventoryService) UpdateStockHandler(ctx *gofr.Context) (interface{}, error) {
-	tileID := ctx.PathParam("id")
-
-	// Assuming a positive or negative integer is sent in the request body for stock update
-	var stockUpdate int
-	if err := ctx.WebSocketConnection.ReadJSON(&stockUpdate); err != nil {
-		return nil, err
-	}
-
-	tile, found := s.Tiles[tileID]
-	if !found {
-		return nil, fmt.Errorf("Tile %v not found)", tileID)
-	}
-
-	// Update stock
-	tile.Stock += stockUpdate
-	s.Tiles[tileID] = tile
-
-	return tile, nil
-}
-
-// ReadTilesHandler handles reading all available tiles.
-func (s *InventoryService) ReadTilesHandler(ctx *gofr.Context) (interface{}, error) {
-	tiles := make([]Tile, 0, len(s.Tiles))
-	for _, tile := range s.Tiles {
-		tiles = append(tiles, tile)
-	}
-	return tiles, nil
-}
-
-// DeleteTileHandler handles deleting finished stock from the database.
-func (s *InventoryService) DeleteTileHandler(ctx *gofr.Context) (interface{}, error) {
-	tileID := ctx.PathParam("id")
-
-	// Check if the tile exists
-	_, found := s.Tiles[tileID]
-	if !found {
-		return nil, fmt.Errorf("Tile %v not found)", tileID)
-	}
-
-	// Delete the tile
-	delete(s.Tiles, tileID)
-
-	return map[string]string{"message": "Tile deleted successfully"}, nil
-}
-
-func main() {
-	app := gofr.New()
-
-	// Initialize the InventoryService
-	inventoryService := &InventoryService{}
-	inventoryService.Initialize()
-
-	// Register handlers
-	app.POST("/tiles", inventoryService.CreateTileHandler)
-	app.PUT("/tiles/:id/stock", inventoryService.UpdateStockHandler)
-	app.GET("/tiles", inventoryService.ReadTilesHandler)
-	app.DELETE("/tiles/:id", inventoryService.DeleteTileHandler)
-
-	// Specify the port for the server to listen on
-	/*port := 8080
-	app.Start()
-
-	// Start the server
-	fmt.Printf("Server is running on port %d...\n", port)
-	*/
-
-	cluster := gocql.NewCluster("localhost")
-	cluster.Port = 2011
-	session, err := cluster.CreateSession()
+	// Create a MongoDB client
+	var err error
+	client, err = mongo.NewClient(clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer session.Close()
-	app.Start()
+
+	// Connect to MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
 }
+
+// Function to create a new student
+func createTile(c *fiber.Ctx) error {
+	var tile Tile
+	if err := c.BodyParser(&tile); err != nil {
+		return err
+	}
+
+	collection := client.Database("testdb").Collection("tiles")
+	result, err := collection.InsertOne(context.TODO(), tile)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(result)
+}
+
+
+// Function to get all tiles
+func getTiles(c *fiber.Ctx) error {
+	var tiles []Tile
+	collection := client.Database("testdb").Collection("tiles")
+	cursor, err := collection.Find(context.TODO(), bson.M{})
+	if err != nil {
+		return err
+	}
+	defer cursor.Close(context.TODO())
+	for cursor.Next(context.TODO()) {
+		var tile Tile
+		cursor.Decode(&tile)
+		tiles = append(tiles, tile)
+	}
+	return c.JSON(tiles)
+}
+
+// Function to get a single tile by ID
+func getTile(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	// Convert the ID string to an ObjectID
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	var tile Tile
+	collection := client.Database("testdb").Collection("tiles")
+	err = collection.FindOne(context.TODO(), bson.M{"_id": objectID}).Decode(&tile)
+	if err != nil {
+		return err
+	}
+	return c.JSON(tile)
+}
+
+// Function to update a tile by ID
+func updateTile(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	// Convert the ID string to an ObjectID
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	var updatedTile Tile
+	if err := c.BodyParser(&updatedTile); err != nil {
+		return err
+	}
+
+	collection := client.Database("testdb").Collection("tiles")
+	filter := bson.M{"_id": objectID}
+	update := bson.D{{"$set", updatedTile}}
+	_, err = collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON("Tile updated successfully")
+}
+
+// Function to delete a tile by ID
+func deleteTile(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	// Convert the ID string to an ObjectID
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	collection := client.Database("testdb").Collection("tiles")
+	filter := bson.M{"_id": objectID}
+	_, err = collection.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON("Tile deleted successfully")
+}
+
+func main() {
+	app := fiber.New()
+
+	// Define CRUD endpoints for tiles
+	app.Get("/tiles", getTiles)
+	app.Get("/tiles/:id", getTile)
+	app.Post("/tiles", createTile)
+	app.Put("/tiles/:id", updateTile)
+	app.Delete("/tiles/:id", deleteTile)
+
+	// Start the server
+	port := 8080
+	fmt.Printf("Server is listening on port %d...\n", port)
+	log.Fatal(app.Listen(fmt.Sprintf(":%d", port)))
+}
+
